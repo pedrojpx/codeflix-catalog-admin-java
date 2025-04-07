3f4ac9712d1d38a62f24a9f2adae611a396daa26
pedrojpx <pjpxavier@gmail.com>
2025-04-07 09:13:02 -0300

    initial commit: creation of project and submodules

---
+++ infrastructure/src/main/java/com/pedrojpx/codeflix/admin/catalog/infrastructure/Main.java
@@ -0,0 +1,16 @@
+package com.pedrojpx.codeflix.admin.catalog.infrastructure;
+
+import com.pedrojpx.codeflix.admin.catalog.application.UseCase;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.print("Hello and welcome!");
+
+        System.out.println(new UseCase().execute());
+
+    }
+}
