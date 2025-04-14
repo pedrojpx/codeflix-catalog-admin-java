43d0be7ce6d4583989f6d6d4d1fc7e2e052af49b
pedrojpx <pjpxavier@gmail.com>
2025-04-14 16:07:30 -0300

    build: Springboot and database basic configuration

--- infrastructure/src/main/java/com/pedrojpx/codeflix/admin/catalog/infrastructure/Main.java
+++ infrastructure/src/main/java/com/pedrojpx/codeflix/admin/catalog/infrastructure/Main.java
@@ -1,14 +1,21 @@
 package com.pedrojpx.codeflix.admin.catalog.infrastructure;
 
-import com.pedrojpx.codeflix.admin.catalog.application.UseCase;
+import com.pedrojpx.codeflix.admin.catalog.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+@SpringBootApplication
 public class Main {
+
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.print("Hello and welcome!");
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
+        SpringApplication.run(WebServerConfig.class, args);
 
 
     }
---
+++ infrastructure/src/main/java/com/pedrojpx/codeflix/admin/catalog/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,10 @@
+package com.pedrojpx.codeflix.admin.catalog.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.pedrojpx.codeflix.admin.catalog")
+public class WebServerConfig {
+
+}
